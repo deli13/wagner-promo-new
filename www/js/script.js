91af4dd3cffd45bf50a3bebfd59a848df1055d3f
Anya <likamoon@list.ru>
2019-06-19 16:51:56 +0300

    Доделан функционал (FullScreen + owl) + верстка шапки

--- www/js/script.js
+++ www/js/script.js
@@ -1,10 +1,23 @@
 $(document).ready(function() { 	
 
-   $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-        anchors: ['page01', 'page02', 'page03'],
-        menu: '#menu',
-        scrollingSpeed: 1000
-      });
+  $('#fullpage').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    anchors: ['page01', 'page02', 'page03', 'page04', 'page05'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+  });
+
+   $("#new-car-slider").owlCarousel({       
+      nav:false,
+      pagination: false,     
+      loop : true,    
+      center : false,
+      items : 1,
+      autoplay: false,
+      lazyLoad: true,      
+      URLhashListener:true,
+      startPosition: 'Mercedes',
+      dotsContainer: '#new-car-slider__nav'
+    });
 
 });
