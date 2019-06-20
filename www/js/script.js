1c509809babf8c78e51ef2e05c8b0e1a8429cd69
Anya <likamoon@list.ru>
2019-06-20 17:54:21 +0300

    Готова десктопная версия

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { 	
 
   $('#fullpage').fullpage({
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    /*sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],*/
     anchors: ['page01', 'page02', 'page03', 'page04', 'page05'],
     menu: '#menu',
     scrollingSpeed: 1000
